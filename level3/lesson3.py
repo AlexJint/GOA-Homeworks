9a6cf514841d16ba9c92e3dbdfb45826dfaaf45e
AlexJint <alexanderjintcharadze@gmail.com>
2024-05-28 21:58:41 +0400

    commit

---
+++ level3/lesson3.py
@@ -0,0 +1,37 @@
+print("Hello Cads")
+print(12)
+# twxt/ტექსტ = string/სტრინგი
+# numder/რიცხვი = integer/ინტეჯერი
+
+
+
+
+name= "dato"
+
+last_name = "janezashvili"
+
+proffesion = "programmer"
+
+print(name, last_name,proffesion)
+
+
+hello_mi_frend = "hi"
+
+
+სახელი = "დათო"
+
+გვარი = "ზანეზაშვილი"
+
+print(სახელი + გვარი)
+
+
+print(50 + 50) # მიმატება +
+print(100 - 50) # გამოკლება -
+
+print(5 * 5) # გამრავლება *
+print(100 / 2) # გაყოფა
+print(5 ** 5) # 5 * 5 *5 * 5 * 5
+# / - გამოიყენება იმისთვის რომ მივიღოთ პასუხი ათწილადი
+# // - გამოიყენება იმისთვის რომ მივიღოთ პასუხი მთელი რიცხვი
+print(15 //2)
+print(100 * 12 )
