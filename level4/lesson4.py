9134344814fd45a6a0d68169723fef4f5180709b
AlexJint <alexanderjintcharadze@gmail.com>
2024-06-07 20:49:25 +0400

    commit

--- level4/lesson4.py
+++ level4/lesson4.py
@@ -5,6 +5,20 @@ print(2 * 5)
 print(10 / 2 )
 print(123 + 100 )
 print(100-233)
-naame= (123)
-name= (1245)
-dato=(132)
+print(5 * 2)
+print(120 / 100 )
+print(100 + 233)
+print(5 * 3)
+print(5 * 4)
+print(5 * 6)
+print(5 * 7)
+print(5 * 7)
+
+
+name = "ალექსანდრე"
+lastname = "ჯინჭარაძე"
+hobbi = "GOA"
+country = "საქართველო"
+
+
+print(name + lastname + hobbi + country)
