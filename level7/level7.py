9840ee938b546d205c805697a2e803bb5ec2df94
AlexJint <alexanderjintcharadze@gmail.com>
2024-06-25 10:50:41 +0400

    commit

---
+++ level7/level7.py
@@ -0,0 +1,68 @@
+print(10 * 10)
+print(25 + 14 )
+print(30 - 15 )
+print(100 / 4 )
+
+
+print("ალექსანდრე")
+print("მირანდა")
+print("კარინა")
+print("ბორისი")
+print("ნინო")
+print("ალექსანდრე")
+print("მარიამი")
+print("დავითი")
+print("ვახტანგი")
+print("მარიამი")
+print("ანისია")
+print("ქეთო")
+print("ალექსანდრე")
+print("ბორისი")
+
+print(8)
+print(9)
+print(34)
+print(41)
+print(38)
+print(63)
+print(65)
+print(42)
+print(8)
+print(3.6)
+print(8)
+print(2)
+print(97)
+print(140)
+print(168)
+
+
+print(28)
+print(29)
+print(54)
+print(61)
+print(58)
+print(83)
+print(85)
+print(62)
+print(28)
+print(23)
+print(28)
+print(22)
+print(117)
+print(260)
+print(188)
+
+
+
+
+
+name = "სახელი"
+lactname = "ჯინჭარაძე"
+color = "მწვანე"
+car = "ფერალი"
+food = 'კვერცხი'
+country = "ბათუმი"
+city = "რუსთაველის 43"
+
+
+name + " " 'lactnsme' + " color" + "car" + "food" + "country" + "citi"
