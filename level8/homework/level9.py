b313667dd3c5810768762af26a826db49c01bd72
AlexJint <alexanderjintcharadze@gmail.com>
2024-07-11 21:56:09 +0400

    commit

---
+++ level8/homework/level9.py
@@ -0,0 +1,25 @@
+savings = float (input("pls cotcr your  namber: "))
+savings2= float (input("pls cotcr your  namber:"))
+
+
+number = float (input("pls cotcr your  namber:"))
+number2 = float (input("pls cotcr your  namber:"))
+
+print(savings + savings2 + number + number2)
+
+
+
+
+
+
+
+
+
+
+
+wins = float (input("enter your of wins: "))
+ties = float (input("enter your of ties: "))
+
+print(wins + ties)
+
+print("score =" + wins + ties)
---
+++ level9/homework/level9.py
@@ -0,0 +1,60 @@
+print(1 < 2)
+print(1 < 3)
+print(1 < 4)
+print(2 < 5)
+print(2 < 6)
+print(4 >= 15)
+print(563 == 3547)
+print(7642 == 4462)
+print(1 < 2)
+print(1 <= 3)
+print(1 >= 4)
+print(2 > 5)
+print(2 > 6)
+print(4 == 15)
+print(563 > 3547)
+print(7642 >= 4462)
+print(1 <= 2)
+print(1 <= 3)
+print(1 <= 4)
+print(2 >= 5)
+print(2 <= 6)
+print(4 >= 15)
+print(563 <= 3547)
+print(7642 >= 4462)
+print(1 > 2)
+print(1 >= 3)
+print(1 <= 4)
+print(2 >= 5)
+print(2 <= 6)
+print(4 >= 15)
+
+
+
+
+
+
+nika = int(input('enter age :'))
+ale = int(input("enter age : "))
+
+print(ale > nika)
+print(ale < nika)
+print(ale == nika)
+print(ale >= nika)
+print(ale <= nika)
+
+
+
+ale = 120
+ale1 = 95
+ale3 = 100
+
+
+print(ale > ale3)
+print(ale1 > ale3)
+
+
+muni = float(input("გთხოვთ დაწერეთ თქვენი ფულის რაოდენობა! :"))
+muni = muni + muni *5/100
+
+print("ერთ წელიწადში გექნება  $" + str(muni))
