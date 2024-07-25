642f9380ac816e5e4036692ad9bd71ab7de50774
AlexJint <alexanderjintcharadze@gmail.com>
2024-07-25 09:49:07 +0400

    commit

---
+++ level11/homework/level11.py
@@ -0,0 +1,23 @@
+print(False > True)
+print(False or True)
+print(True and True)
+print(False or False)
+
+
+
+
+print(6 < 5)   #მეტობის ნიშანი აღნიშნავს რომელია მეტი 
+print(5665 > 3)  #ნაკლებობის ნიშანი აღნიშნავს რომელია ნაკლები 
+print(6 == 5) 
+print(5665 <= 3)
+print(6 >= 5)
+
+print(6 + 5) #მიმატება ამატებს
+print(5665 - 3) #გამოკლება აკლებს
+print(6 / 5)   #/ ნისნავს დაყოფას
+print(5665 * 3) # * ნიშნავს გამრავლებას
+print(6 // 5)
+print(7 == 134675)
+
+
+
