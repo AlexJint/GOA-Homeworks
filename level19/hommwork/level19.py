895f7059ec789c47a15e3261cd9c7387818e4cb7
AlexJint <alexanderjintcharadze@gmail.com>
2024-09-13 09:55:02 +0400

    commit

---
+++ level19/hommwork/level19.py
@@ -0,0 +1,17 @@
+number = str(input("enter you namber"))
+
+
+for a in range(0 , 10 + 1 , 2):
+    print(str (a) + 'hi' )
+
+
+
+for b in range(0 , 16 + 8 , 7):
+    print(str (b) + 'name' )
+
+
+
+
+for d in range(0 , 16 + 8 , 7):
+    print(str (d) + 'last name' )
+    
