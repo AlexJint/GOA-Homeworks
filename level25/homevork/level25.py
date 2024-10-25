654064623f2d0d40c1740b31a380ca351fe7309a
AlexJint <alexanderjintcharadze@gmail.com>
2024-10-25 10:58:20 +0400

    commit

---
+++ level25/homevork/level25.py
@@ -0,0 +1,28 @@
+Values = ["hi" , 'text' , "georgia" , "in" , "GOA" , "harry" , "projecs" , "gpp" ]
+
+print(len(Values))
+
+Values.append("i love GOA")
+
+print(Values)
+
+Values.insert(3, "alex")
+
+print(Values)
+
+print(Values.index('in'))
+
+num = (2473 , 12 ,8483, 9735, 7945, 6533, 46, 8946, 87,)
+
+print(max(num))
+print(min(num))
+
+num.reverse()
+print(num)
+
+
+num.remove(10)
+print(num)
+
+num2 = (10, 20, 30, 40, 50, 50, 70, 80, 90, 100)
+print(num.count(40))
