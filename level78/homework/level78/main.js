d3477d216430a17b2f86726c6756641db7643ab8
AlexJint <alexanderjintcharadze@gmail.com>
2025-10-17 19:15:54 +0400

    first commit

---
+++ level78/homework/level78/main.js
@@ -0,0 +1,10 @@
+import { sum } from './app.js';
+console.log("3 + 5 =", sum(3, 5));
+
+import { sum } from './app.js';
+
+console.log("3 + 5 =", sum(3, 5));
+
+
+const result = sum(10, 20);
+console.log("10 + 20 =", result);
